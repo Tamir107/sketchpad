8ef1072c01242c1127bea2ab2bd2c0b456fb530b
Tamir Razon <tamir.razon@gmail.com>
2024-02-08 19:06:16 +0200

    Add grid layout and mousehover functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const container = document.querySelector("#container");
+const rowArray = [];
+for (let i = 0; i < 16; i++) {
+    const newRow = document.createElement("div");
+    rowArray.push(newRow);
+    container.appendChild(newRow);
+    for (let j = 0; j < 16; j++) {
+        const newCol = document.createElement("div");
+        newCol.classList.add("cell");
+        newRow.appendChild(newCol);
+    }
+}
+
+container.addEventListener("mouseover",(e) => {
+    if(e.target.id === "container") return;
+    e.target.style.background = "red";
+})
